6309d2a7064e922663ba358795870e101570e188
SuiLing237 <54572003+SuiLing237@users.noreply.github.com>
2021-03-02 01:23:56 +0800

    Save contacts to file, Next Button

--- RescueMe/app/src/main/java/com/myapplication/rescueme/MainActivity.kt
+++ RescueMe/app/src/main/java/com/myapplication/rescueme/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.myapplication.rescueme
 
-import android.R
 import android.content.Intent
 import android.os.Bundle
 import android.provider.ContactsContract
@@ -8,21 +7,26 @@ import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.myapplication.rescueme.databinding.ActivityHomeBinding
 import com.myapplication.rescueme.databinding.ActivityMainBinding
+import java.io.PrintStream
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
     private val PICK_CONTACT = 1
 
-    private var contactToDetails = HashMap<String, ArrayList<String>>()
-    private var details = ArrayList<String>()
-    private lateinit var myAdapter: ArrayAdapter<String>
+    private lateinit var myAdapter:ArrayAdapter<String>
+    private var contactsList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupContactsList()
     }
 
     fun goToContacts(view: View) {
@@ -36,11 +40,13 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == PICK_CONTACT && resultCode == RESULT_OK && data != null) {
             val contactData = data.data!!
             val cursor1 = contentResolver.query(contactData, null, null, null, null)
+            var details = ArrayList<String>()
 
             if (cursor1 != null && cursor1.moveToFirst()) {
                 val contactId = cursor1.getString(cursor1.getColumnIndex(ContactsContract.Contacts._ID))
                 val contactName = cursor1.getString(cursor1.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
                 val idResults = cursor1.getString(cursor1.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)).toInt()
+                details.add(contactId)
                 details.add(contactName)
 
                 // checks if contact has phone number
@@ -60,18 +66,83 @@ class MainActivity : AppCompatActivity() {
                         Toast.makeText(this, "Name: $contactName, PhoneNumber: $contactNumber", Toast.LENGTH_LONG).show()
                     }
                     cursor2.close()
-
-                    // details will be [name, number1, number2, ...]
-                    contactToDetails[contactId] = details
                 }
-
-                // TODO: Display name and contactNumber as one list item. Look into custom adapters.
-                myAdapter = ArrayAdapter(this, R.layout.simple_list_item_2, R.id.text1, details)
-                binding.contactsLv.adapter = myAdapter
-
                 cursor1.close()
+                writeFile(details)
+            }
+        }
+    }
+
+    private fun writeFile(details : ArrayList<String>) {
+        var contactExists = false
+
+        // check if file exist before reading.
+        if (fileExist("contacts.txt")) {
+            val scan = Scanner(openFileInput("contacts.txt"))
+
+            while (scan.hasNextLine()) {
+                val line = scan.nextLine()
+                val contactDetails = line.split("\t")
+
+                // check if contactId in file. If yes, stop reading.
+                if (contactDetails[0] == details[0]) {
+                    contactExists = true
+                    break
+                }
             }
         }
 
+        // Only write if contact does not exist.
+        if (!contactExists) {
+            val output = PrintStream(openFileOutput("contacts.txt", MODE_APPEND))
+            output.println(details.joinToString(separator = "\t"))
+            output.close()
+            setupContactsList()
+            Toast.makeText(this, "Contact added successfully!", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "This contact has already been chosen!", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun fileExist(fname: String?): Boolean {
+        val file = baseContext.getFileStreamPath(fname)
+        return file.exists()
+    }
+
+    private fun setupContactsList() {
+        if (!fileExist("contacts.txt")) {
+            return
+        }
+
+        val scan = Scanner(openFileInput("contacts.txt"))
+        while (scan.hasNextLine()) {
+            val line = scan.nextLine()
+            val pieces = line.split("\t")
+            val contactName = pieces[1]
+            val contactNumber = pieces[2]
+
+            // append pieces as a string in this format "Name\nContactNumber"
+            val contactDetails = "$contactName\n$contactNumber"
+            if (!contactsList.contains(contactDetails)) {
+                contactsList.add(contactDetails)
+            }
+        }
+
+        myAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, contactsList)
+        binding.contactsLv.adapter = myAdapter
+
+        showButton()
+    }
+
+    fun goToHome(view: View) {
+        val it = Intent(this, HomeActivity::class.java)
+        startActivity(it)
+    }
+
+    // show button only if contacts is minimally 3
+    private fun showButton() {
+        if (contactsList.size >= 3) {
+            binding.nextBtn.visibility = View.VISIBLE
+        }
     }
 }
