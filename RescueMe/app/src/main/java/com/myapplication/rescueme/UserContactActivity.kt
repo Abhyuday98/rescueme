78fdd1504492fc881dbaf42ad68ef3b2e9a7767d
SuiLing237 <54572003+SuiLing237@users.noreply.github.com>
2021-03-31 19:32:29 +0800

    Add user name and save to file

--- RescueMe/app/src/main/java/com/myapplication/rescueme/UserContactActivity.kt
+++ RescueMe/app/src/main/java/com/myapplication/rescueme/UserContactActivity.kt
@@ -18,19 +18,22 @@ class UserContactActivity : AppCompatActivity() {
         val contactNumberEt = findViewById<EditText>(R.id.contactNumberEt)
         val contactNumber = contactNumberEt.text.toString()
 
+        val nameEt = findViewById<EditText>(R.id.nameEt)
+        val name = nameEt.text.toString()
+
         // 11 characters -> +6512345678
-        if (contactNumber.length == 11) {
-            saveContactNumber(contactNumber)
+        if (contactNumber.length == 11 && name.isNotEmpty()) {
+            saveContactNumber(name, contactNumber)
             val it = Intent(this, FirstPasscodeActivity::class.java)
             startActivity(it)
         } else {
-            Toast.makeText(this, "Please enter a valid contact number.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Please enter a valid name/contact number.", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun saveContactNumber(contactNumber : String) {
+    private fun saveContactNumber(name : String, contactNumber : String) {
         val output = PrintStream(openFileOutput("my_contact.txt", MODE_PRIVATE))
-        output.println(contactNumber)
+        output.println(name + "\t" + contactNumber)
         output.close()
     }
 }
