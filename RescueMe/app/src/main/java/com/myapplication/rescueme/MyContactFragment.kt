b6a32183c72010f1aa9847c0cfbf5dacacb542ae
SuiLing237 <54572003+SuiLing237@users.noreply.github.com>
2021-03-31 19:33:02 +0800

    Display current contact details and allow update contact

--- RescueMe/app/src/main/java/com/myapplication/rescueme/MyContactFragment.kt
+++ RescueMe/app/src/main/java/com/myapplication/rescueme/MyContactFragment.kt
@@ -6,72 +6,86 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_mycontact.*
+import org.w3c.dom.Text
 import java.io.PrintStream
 import java.security.MessageDigest
 import java.util.*
 
 class MyContactFragment : Fragment(), View.OnClickListener {
+    private lateinit var v: View
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val v = inflater.inflate(R.layout.fragment_mycontact, container, false)
+        v = inflater.inflate(R.layout.fragment_mycontact, container, false)
 
         val saveContactBtn = v.findViewById<Button>(R.id.saveContactBtn)
         saveContactBtn.setOnClickListener(this)
 
+        getCurrentContactDetails()
+
         return v
     }
 
-    private fun saveContactNumber(contactNumber : String) {
+    private fun getCurrentContactDetails() {
+        val currentContactTextView = v.findViewById<TextView>(R.id.currentContactTextView)
+
+        if (!fileExist("my_contact.txt")) {
+            currentContactTextView.text = "No contact details."
+            return
+        }
+
+        var name = ""
+        var contactNumber = ""
+
+        val scan = Scanner(activity!!.openFileInput("my_contact.txt"))
+
+        while (scan.hasNextLine()) {
+            val line = scan.nextLine()
+            val pieces = line.split("\t")
+
+            name = pieces[0]
+            contactNumber = pieces[1]
+
+            currentContactTextView.text = "Current contact details:\n$name\n$contactNumber"
+        }
+    }
+
+    private fun saveContactNumber(name : String, contactNumber : String) {
         val output = PrintStream(activity!!.openFileOutput("my_contact.txt", AppCompatActivity.MODE_PRIVATE))
-        output.println(contactNumber)
+        output.println(name + "\t" + contactNumber)
         output.close()
     }
 
-    private fun isCorrectContact(contactNumber: String) : Boolean {
-        var currentContactNumber = ""
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.saveContactBtn -> {
+                val nameEditText = activity!!.findViewById<EditText>(R.id.nameEditText)
+                val name = nameEditText.text.toString()
 
-        val scan = Scanner(activity!!.openFileInput("my_contact.txt"))
-        while (scan.hasNextLine()) {
-            currentContactNumber = scan.nextLine()
+                val contactEditText = activity!!.findViewById<EditText>(R.id.contactEditText)
+                val contactNumber = contactEditText.text.toString()
+
+                if (name.isNotEmpty() && contactNumber.length == 11) {
+                    saveContactNumber(name, contactNumber)
+                    getCurrentContactDetails()
+                    Toast.makeText(activity!!, "Contact details successfully updated.", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(activity!!, "Please enter a valid name/contact number.", Toast.LENGTH_SHORT).show()
+                }
+
+                nameEditText.text.clear()
+                contactEditText.text.clear()
+            }
         }
-
-        return currentContactNumber == contactNumber
     }
 
-    override fun onClick(v: View?) {
-        val oldContactEditText =  activity!!.findViewById<EditText>(R.id.oldContactEditText)
-        val oldContact = oldContactEditText.text.toString()
-
-        val newContactEditText = activity!!.findViewById<EditText>(R.id.newContactEditText)
-        val newContact = newContactEditText.text.toString()
-
-        val confirmNewContactEditText = activity!!.findViewById<EditText>(R.id.confirmNewContactEditText)
-        val confirmNewContact = confirmNewContactEditText.text.toString()
-
-        var errorMsg = ""
-        if (v!!.id == R.id.saveContactBtn) {
-            if (!isCorrectContact(oldContact)) {
-                errorMsg += "Old contact entered is incorrect.\n"
-            }
-
-            if (newContact.length != 11) {
-                errorMsg += "Please enter a valid new contact.\n"
-            }
-
-            if(newContact != confirmNewContact) {
-                errorMsg += "Confirm new contact does not match.\n"
-            }
-
-            if (isCorrectContact(oldContact) && newContact.length == 11 && newContact == confirmNewContact) {
-                saveContactNumber(newContact)
-                Toast.makeText(activity!!, "Change my contact is successful.", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(activity!!, errorMsg.dropLast(2), Toast.LENGTH_LONG).show()
-            }
-        }
+    private fun fileExist(fname: String?): Boolean {
+        val file = activity!!.baseContext.getFileStreamPath(fname)
+        return file.exists()
     }
 
 }
